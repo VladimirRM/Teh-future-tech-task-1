3cc20e933c05990efe48d37f2c04b236f99adbf6
Vladimir RM <shvdrummer@gmail.com>
2023-01-05 14:05:20 +0200

    add usest & use Eff

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios'
+import './App.css'
+
 
 import './App.css';
 
 function App() {
+
+  const [booksData,setBooksData]= useState([])
+React.useEffect(()=>{
+  
+})
+
+
   return (
     <div className="wrapper">
     <div className="app">
       <div className="app__body">
-        
+
       </div>
 
 
